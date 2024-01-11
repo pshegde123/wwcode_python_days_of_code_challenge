c3ceb053ccd3e27c123833df8cdbaf41e69b0d50
Pradnya Hegde <pradnyahegde@gmail.com>
2024-01-10 19:45:03 -0500

    Swap values of two variables

---
+++ Day1/swapvariables.py
@@ -0,0 +1,14 @@
+x = 5
+y = 10
+
+print("Before swap x,y = ",x,y)
+x,y = y,x
+print("After swap x,y = ",x,y)
+
+a = 10
+b = 20
+print("Before swap a,b = ",a,b)
+temp = a
+a = b
+b = temp
+print("After swap a,b = ",a,b)
